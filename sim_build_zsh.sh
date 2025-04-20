b4cd2dd8e7162e7add5567bdaa3e6803d5da09a6
Vivek Singh <vivek.mitmpl2023@learner.manipal.edu>
2025-04-20 19:31:09 +0530

    sim_build_zsh added

---
+++ sim_build_zsh.sh
@@ -0,0 +1,3 @@
+colcon build --packages-ignore direct_lidar_odometry direct_lidar_inertial_odometry zed_ros2 zed_components zed_wrapper ouster_ros behaviour_manager
+. ./install/setup.zsh
+colcon build --packages-select behaviour_manager
