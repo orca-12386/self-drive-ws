5259cf29cdfebdaa895d794d7e5e610114a26bc6
codegallivant <janak.sh41@gmail.com>
2025-04-26 08:17:46 +0530

    combine bash, zsh build/source scripts

--- sim_build.sh
+++ sim_build.sh
@@ -1,6 +1,13 @@
+source_setup () {
+    if [ -n "$ZSH_VERSION" ]; then
+        . install/setup.zsh
+    elif [ -n "$BASH_VERSION" ]; then
+        . install/setup.bash
+    fi
+}
 export MAKEFLAGS="-j 2"
 colcon build --packages-select interfaces
-. install/setup.bash
+source_setup
 colcon build --packages-ignore interfaces direct_lidar_odometry direct_lidar_inertial_odometry zed_ros2 zed_components zed_wrapper ouster_ros behaviour_manager ouster_ros --executor sequential
-. install/setup.bash
+source_setup
 
