9e0e2794be696ecb744baf7acb15aa044b054b57
codegallivant <janak.sh41@gmail.com>
2025-04-26 13:45:35 +0530

    update build scripts

--- irl_build.sh
+++ irl_build.sh
@@ -8,6 +8,5 @@ source_setup () {
 export MAKEFLAGS="-j 2"
 colcon build --packages-select interfaces
 source_setup
-colcon build --packages-ignore interfaces direct_lidar_odometry direct_lidar_inertial_odometry zed_ros2 zed_components zed_wrapper ouster_ros ouster_ros --executor sequential
+colcon build --packages-ignore interfaces --executor sequential
 source_setup
-
