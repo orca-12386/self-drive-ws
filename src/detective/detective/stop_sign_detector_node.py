716145fad1608cb705e29410b8ae8446104e2c3c
MrigaankMouli <sufy1707@gmail.com>
2025-05-26 17:03:50 -0400

    added cloud point to global point transforms to stop sign detection

--- src/detective/detective/stop_sign_detector_node.py
+++ src/detective/detective/stop_sign_detector_node.py
@@ -2,7 +2,20 @@ import cv2
 import rclpy
 import pytesseract
 from detective.detector_base import BaseDetector
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import PointStamped
+from nav_msgs.msg import Odometry
+import numpy as np
+from scipy.spatial.transform import Rotation
+from math import sin, cos, pi
+import tf2_ros
+import tf2_geometry_msgs
 
+def euler_to_quat(euler):
+    return Rotation.from_euler('xyz', euler).as_quat()
+
+def quat_to_euler(quat):
+    return Rotation.from_quat(quat).as_euler('xyz') 
 
 class StopSignDetector(BaseDetector):
     def __init__(self):
@@ -10,6 +23,29 @@ class StopSignDetector(BaseDetector):
         
         # Override model path for stop signs
         self._load_model()  # Reload model with new path
+        self.odom_sub = self.create_subscription(Odometry,"/odom", self.odom_callback, 10)
+        self.depth_sub = self.create_subscription(Image, "/zed_node/stereocamera/depth/image_raw", self.depth_callback, 10)
+        self.pointstamped_pub = self.create_publisher(PointStamped, "detector/stop_sign/position", 10)
+        self.sim = True
+        self.detected_point = None
+        self.depth_stamp = None
+        self.depth_frame_id = None
+        self.latest_depth_image = None
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+        self.transform_stamped = None
+        self.bot_position = None
+        self.bot_orientation = None
+
+
+    def depth_callback(self, msg):
+        self.depth_stamp = msg.header.stamp
+        self.depth_frame_id = msg.header.frame_id
+        self.latest_depth_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+
+    def odom_callback(self, msg):
+        self.bot_position = msg.pose.pose.position
+        self.bot_orientation = msg.pose.pose.orientation
     
     def detect_objects(self):
         if self.latest_image is None:
@@ -59,6 +95,50 @@ class StopSignDetector(BaseDetector):
                 0 <= y < self.latest_depth_image.shape[0]):
                 depth_value = self.latest_depth_image[y, x]
                 point = self._pixel_to_point(x, y, depth_value)
+
+                if point:
+                    if self.sim:
+                        pitch = float(23.5) * pi / float(180)
+                        point.y = float(1.5) - point.z*sin(pitch) - point.y*cos(pitch)
+                    
+                    point.y, point.z = point.z, point.y  
+                    point_in = PointStamped()
+                    point_out = PointStamped()
+                    point_in.header.stamp = self.depth_stamp
+                    point_in.header.frame_id = "base_link"
+                    point_in.point.x = point.x
+                    point_in.point.y = point.y
+                    point_in.point.z = point.z
+
+                    if not self.sim:
+                        try:
+                            point_out = tf2_geometry_msgs.do_transform_point(point_in, self.transform_stamped)
+                            point.x = point_out.point.x
+                            point.y = point_out.point.y
+                            point.z = point_out.point.z
+                        except tf2_ros.TransformException as e:
+                            self.get_logger().error(f"Transform error: {e}")
+                            return
+
+                    if self.bot_orientation is not None:
+                        euler = quat_to_euler([self.bot_orientation.x, self.bot_orientation.y, 
+                                             self.bot_orientation.z, self.bot_orientation.w])
+                        bot_yaw = euler[2]  
+
+                        self.detected_point = PointStamped()
+                        self.detected_point.header.stamp = self.get_clock().now().to_msg()
+                        self.detected_point.header.frame_id = "map"
+                        self.detected_point.point.x = self.bot_position.x + point.x * sin(bot_yaw) + point.y * cos(bot_yaw)
+                        self.detected_point.point.y = self.bot_position.y + point.y * sin(bot_yaw) - point.x * cos(bot_yaw) 
+                        self.detected_point.point.z = - point.z
+
+                        if self.detected_point.point.z > 1:
+                            self.get_logger().info("Detected point height is too high(prolly the sky).")
+                            return 
+                        
+                        self.point_pub.publish(point)
+                        self.pointstamped_pub.publish(self.detected_point)
+                        self.get_logger().info(f"Detected Pothole: {self.detected_point.point}")
                 
                 if point:
                     self.point_pub.publish(point)
